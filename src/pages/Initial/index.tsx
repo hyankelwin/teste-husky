b8bdda4a7e61dfc98b5b920a4b1d1b3bad18d079
Hyan Kelwin Souza Campos <hyan.campos@luizalabs.com>
2021-05-13 15:20:58 -0300

    test 10

--- src/pages/Initial/index.tsx
+++ src/pages/Initial/index.tsx
@@ -5,6 +5,9 @@ const Initial: React.FC = () => {
   return (
     <View>
       <Text>Bem vindo à Expedição</Text>
+      <View>
+        <Text>Novaaaa e tals</Text>
+      </View>
     </View>
   );
 };
